1082a932bbc9bf5dc3dd63969509735fab5a60b6
lorranyoliveira <lolo.souza80@yahoo.com>
2023-01-19 20:29:42 -0300

    Adiciona arquivo responsável pelo log

---
+++ src/log.cpp
@@ -0,0 +1,36 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <ctime>
+
+static std::string file_name = "./log.csv";
+static std::ofstream file;
+
+void create_file_log(){
+    file.open(file_name);
+    if (file.is_open()) {
+        std::cout << "The file is open." << std::endl;
+    } else {
+        std::cout << "The file is not open." << std::endl;
+    }
+}
+
+void close_file_log()
+{
+    file.close();
+    
+    if (file.is_open()) {
+        std::cout << "Erro ao fechar o arquivo." << std::endl;
+        file.close();
+
+    } 
+}
+
+void write_in_file_log(std::string message)
+{
+    std::time_t result = std::time(nullptr);
+    std::string time_now = std::asctime(std::localtime(&result));
+
+    file << time_now << message;
+
+}
