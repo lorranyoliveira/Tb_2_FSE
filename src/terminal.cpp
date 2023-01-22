36b6ecc6255b217d19c82592738f67ff236a115f
lorranyoliveira <lolo.souza80@yahoo.com>
2023-01-22 01:33:28 -0300

    Corrige função terminal

--- src/terminal.cpp
+++ src/terminal.cpp
@@ -25,30 +25,4 @@ void terminal_() {
 
     pid_configura_constantes(kp, ki, kd);
 
-    while(1)
-    {
-        temp_referencia = get_info(0xC2, 0x23);
-
-        pid_atualiza_referencia(temp_referencia);
-        
-        temp_interna = get_info(0xC1, 0x23);
-
-        intensidade = pid_controle(temp_interna);
-
-        control_temp(intensidade);
-
-        temp_ambiente = getTemp_Ambiente();
-
-        write_in_file_log(temp_interna, temp_ambiente, temp_referencia);        
-
-        system("clear");
-        printf("Temperatura interna: %f\n", temp_interna);
-        printf("Temperatura ambiente: %f\n", temp_ambiente);
-        printf("Temperatura referencia: %f\n", temp_referencia);
-        sleep(2);
-
-    }
-
-
-
 }
